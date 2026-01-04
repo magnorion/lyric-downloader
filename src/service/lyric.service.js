df5ca1f8c6a38bc30ce8c21ccfa362174328798c
magnorion <lucas_front@yahoo.com.br>
2026-01-03 21:05:24 -0300

    feat: fix async call for service

--- src/service/lyric.service.js
+++ src/service/lyric.service.js
@@ -3,20 +3,21 @@ const axios = require('axios');
 class LyricService {
     apiUrl = process.env.API_URL;
 
-    getLyric(track_name, artist_name, album_name, duration) {
-        return axios.get(`${this.apiUrl}/api/get`, {
-            params: {
-                track_name,
-                artist_name,
-                album_name,
-                duration
-            }
-        }).then(response => {
+    async getLyric(track_name, artist_name, album_name, duration) {
+        try {
+            const response = await axios.get(`${this.apiUrl}/api/get`, {
+                params: {
+                    track_name,
+                    artist_name,
+                    album_name,
+                    duration
+                }
+            });
             return response.data;
-        }).catch(error => {
-            console.error("Error fetching lyrics:", error);
+        } catch (error) {
+            console.error("😵‍💫 Error fetching lyrics", {track_name, artist_name, album_name, duration});
             throw error;
-        });
+        }
     }
 }
 
